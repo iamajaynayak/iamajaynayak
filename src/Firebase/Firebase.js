242a235940ae2a9d88c81d5e021c25615a29372f
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2020-12-13 17:43:46 +0530

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-const config = {
-  apiKey: "AIzaSyDrOYEiZg-cxHdyikaA0WVKQIYcpGiqbXY",
-  authDomain: "myportfolio-c651a.firebaseapp.com",
-  databaseURL: "https://myportfolio-c651a.firebaseio.com",
-  projectId: "myportfolio-c651a",
-  storageBucket: "myportfolio-c651a.appspot.com",
-  messagingSenderId: "678468758415",
-  appId: "1:678468758415:web:08d480baa6e1ba5e74b9fa",
-};
-
+const config = { }
 firebase.initializeApp(config);
 
 export const db = firebase.firestore();
